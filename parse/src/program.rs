6a281b4fc8e16e147e598b0f693a77c544b313fb
rhysormond <git@rhysormond.com>
2021-05-06 22:58:02 -0700

    feat: implement image parsing

--- parse/src/program.rs
+++ parse/src/program.rs
@@ -4,22 +4,27 @@ use std::collections::HashMap;
 use image::GenericImageView;
 use crate::codel::Codel;
 
+#[derive(Debug)]
 pub struct Program {
-    // source: HashMap<(u32, u32), >
+    codels: HashMap<(u32, u32), Codel>,
+    height: u32,
+    width: u32,
 }
 
 impl Program {
-    pub fn load(path: &str) -> image::DynamicImage {
+    pub fn load(path: &str) -> Program {
         let img = image::open(path).unwrap();
 
-        let result: HashMap<(u32, u32), Codel> = img.pixels().into_iter().map( | (x, y, color) | {
+        let (width, height) = img.dimensions();
+
+        let codels: HashMap<(u32, u32), Codel> = img.pixels().into_iter().map( | (x, y, color) | {
             ((x, y), Codel::from(color))
         }).collect();
-        println!("{:?}", result);
-        img
-        //     .map | (x, y, pixel) | {
-        //     pixel
-        // }
-    }
 
+        Program {
+            codels,
+            width,
+            height
+        }
+    }
 }
