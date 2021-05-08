a6cc998ea4b08762da9c46a0871c2d83b552fa14
rhysormond <git@rhysormond.com>
2021-05-07 20:58:44 -0700

    refactor: program as 2d vec instead of hashmap

--- parse/src/program.rs
+++ parse/src/program.rs
@@ -1,34 +1,41 @@
 extern crate image;
-
-use std::collections::HashMap;
+extern crate itertools;
 
 use image::GenericImageView;
+use itertools::Itertools;
 
 use crate::codel::Codel;
 
 /// A Piet program is represented as a 2d grid of Codels.
+///
+/// # Parameters
+///
+/// * `codels` - A collection of rows of Codels where `codels[0][0]` represents the top-left Codel.
+/// * `rows` - The number of rows in the program.
+/// * `cols` - The number of cols in the program.
 #[derive(Debug)]
 pub struct Program {
-    codels: HashMap<(u32, u32), Codel>,
-    height: u32,
-    width: u32,
+    codels: Vec<Vec<Codel>>,
+    rows: u32,
+    cols: u32,
 }
 
 impl Program {
     /// Loads a program from a file given its path.
     pub fn load(path: &str) -> Program {
         let img = image::open(path).unwrap();
-
-        let (width, height) = img.dimensions();
-
-        let codels: HashMap<(u32, u32), Codel> = img.pixels().into_iter().map(|(x, y, color)| {
-            ((x, y), Codel::from(color))
-        }).collect();
+        let (cols, rows) = img.dimensions();
+        let codels: Vec<Vec<Codel>> = img
+            .pixels()
+            .chunks(cols as usize)
+            .into_iter()
+            .map(|row| row.map(|(.., color)| Codel::from(color)).collect())
+            .collect();
 
         Program {
             codels,
-            width,
-            height,
+            cols,
+            rows,
         }
     }
 }
