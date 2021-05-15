b86906a85e582c927f581d8b90f46f7674bc6ac7
rhysormond <git@rhysormond.com>
2021-05-14 23:36:07 -0700

    refactor: bake directions into program

--- interpret/src/direction.rs
+++ interpret/src/direction.rs
@@ -1,3 +1,5 @@
+use parse::direction::ProgramDirection;
+
 /// Possible directions for the direction pointer.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub(crate) enum PointerDirection {
@@ -8,12 +10,37 @@ pub(crate) enum PointerDirection {
 }
 
 impl PointerDirection {
+    /// The next direction in clockwise order
     pub fn next(&self) -> Self {
         match self {
             PointerDirection::Up => PointerDirection::Right,
             PointerDirection::Right => PointerDirection::Down,
             PointerDirection::Down => PointerDirection::Left,
             PointerDirection::Left => PointerDirection::Up,
+        }
+    }
+
+    /// The previous direction in clockwise order
+    fn previous(&self) -> Self {
+        self.next().next().next()
+    }
+
+    /// The direction after applying a chooser direction
+    pub fn with_chooser(&self, chooser: ChooserDirection) -> Self {
+        match chooser {
+            ChooserDirection::Left => self.previous(),
+            ChooserDirection::Right => self.next(),
+        }
+    }
+}
+
+impl From<PointerDirection> for ProgramDirection {
+    fn from(from: PointerDirection) -> ProgramDirection {
+        match from {
+            PointerDirection::Up => ProgramDirection::Up,
+            PointerDirection::Right => ProgramDirection::Right,
+            PointerDirection::Down => ProgramDirection::Down,
+            PointerDirection::Left => ProgramDirection::Left,
         }
     }
 }
@@ -26,6 +53,7 @@ pub(crate) enum ChooserDirection {
 }
 
 impl ChooserDirection {
+    /// The next/opposite direction
     pub fn next(&self) -> Self {
         match self {
             ChooserDirection::Left => ChooserDirection::Right,
---
+++ parse/src/direction.rs
@@ -0,0 +1,8 @@
+/// Cardinal directions within the program.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum ProgramDirection {
+    Up,
+    Right,
+    Down,
+    Left,
+}
