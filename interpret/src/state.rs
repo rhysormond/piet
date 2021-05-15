bf39c0a77fea75057c5ffaf39a241a806107b69b
rhysormond <git@rhysormond.com>
2021-05-14 23:38:11 -0700

    refactor: squash pointer location into tuple

--- interpret/src/state.rs
+++ interpret/src/state.rs
@@ -4,15 +4,14 @@ use crate::direction::{ChooserDirection, PointerDirection};
 ///
 /// # Parameters
 ///
-/// * `pointer_x` - The x coordinate of the direction pointer.
-/// * `pointer_y` - The y coordinate of the direction pointer.
+/// * `pointer_location` - The (row, column) coordinates of the direction pointer (relative to the top left corner).
 /// * `pointer_direction` - The direction of the direction pointer.
 /// * `chooser_direction` - The direction of the direction chooser.
 /// * `stack` - The stack for storing data values.
 /// * `termination_counter` - The number of times that the program has failed to advance.
+#[derive(Debug)]
 pub struct State {
-    pub(crate) pointer_x: usize,
-    pub(crate) pointer_y: usize,
+    pub(crate) pointer_location: (usize, usize),
     pub(crate) pointer_direction: PointerDirection,
     pub(crate) chooser_direction: ChooserDirection,
     pub(crate) stack: Vec<isize>,
@@ -22,8 +21,7 @@ pub struct State {
 impl State {
     pub fn new() -> Self {
         Self {
-            pointer_x: 0,
-            pointer_y: 0,
+            pointer_location: (0, 0),
             pointer_direction: PointerDirection::Right,
             chooser_direction: ChooserDirection::Left,
             stack: Vec::new(),
