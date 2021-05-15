bd01abcf93e642dde5e927e4795de307e5471ab5
rhysormond <git@rhysormond.com>
2021-05-14 21:26:16 -0700

    refactor: ditch some eager region computations
    We definitely want these eventually but the right interface isn't super obvious yet.

--- parse/src/lib.rs
+++ parse/src/lib.rs
@@ -2,4 +2,3 @@
 
 pub mod program;
 pub mod codel;
-pub mod region;
--- parse/src/program.rs
+++ parse/src/program.rs
@@ -8,22 +8,17 @@ use image::GenericImageView;
 use itertools::Itertools;
 
 use crate::codel::Codel;
-use crate::region::Region;
 
 /// A Piet program is represented as a 2d grid of Codels.
 ///
 /// # Parameters
 ///
 /// * `codels` - A collection of rows of Codels where `codels[0][0]` represents the top-left Codel.
-/// * `rows` - The number of rows in the program.
-/// * `cols` - The number of cols in the program.
+/// * `regions` - A map of coordinates to the size of their corresponding regions.
 #[derive(Debug)]
 pub struct Program {
-    region_map: HashMap<(usize, usize), usize>,
-    regions: Vec<Region>,
     codels: Vec<Vec<Codel>>,
-    rows: usize,
-    cols: usize,
+    regions: HashMap<(usize, usize), usize>,
 }
 
 impl Program {
@@ -41,48 +36,35 @@ impl Program {
             .map(|row| row.map(|(.., color)| Codel::from(color)).collect())
             .collect();
 
-        let (regions, region_map) = Self::get_regions(&codels, &rows, &cols);
+        let regions = Self::get_regions(&codels, &rows, &cols);
 
         Program {
-            region_map,
             regions,
             codels,
-            cols,
-            rows,
         }
     }
 
-    fn get_regions(codels: &Vec<Vec<Codel>>, rows: &usize, cols: &usize) -> (Vec<Region>, HashMap<(usize, usize), usize>) {
-        // All of the codels that have already been incorporated into regions
-        let mut seen = HashSet::new();
-        // All of the seen regions
-        let mut regions = Vec::new();
-        // Maps codel coordinates to the index of the region they belong to
-        let mut region_map = HashMap::new();
+    /// Builds a map of program coordinates to the sizes of their corresponding regions
+    fn get_regions(codels: &Vec<Vec<Codel>>, rows: &usize, cols: &usize) -> HashMap<(usize, usize), usize> {
+        // Maps codel coordinates to the index of the size of the region that they belong to
+        let mut regions = HashMap::new();
         for row in 0..*rows {
             for col in 0..*cols {
-                let new = !seen.contains(&(row, col));
-                let colored = match &codels[row][col] {
-                    Codel::Color { .. } => true,
-                    Codel::Black | Codel::White => false,
-                };
-                // Build a region if the codel hasn't been seen and it has a color
-                if new && colored {
-                    let (region, coordinates) = Self::get_region(codels, (row, col));
-                    regions.push(region);
-                    let region_idx = regions.len();
-                    for point in coordinates {
-                        seen.insert(point);
-                        region_map.insert(point, region_idx);
+                // Build a region if the codel hasn't been seen before
+                if !regions.contains_key(&(row, col)) {
+                    let members = Self::get_region(codels, (row, col));
+                    let count = members.len();
+                    for point in members {
+                        regions.insert(point, count);
                     }
                 }
             }
         }
-        (regions, region_map)
+        regions
     }
 
-    /// Build a contiguous region of the same color
-    fn get_region(codels: &Vec<Vec<Codel>>, point: (usize, usize)) -> (Region, HashSet<(usize, usize)>) {
+    /// Get all members of the same contiguous region of color
+    fn get_region(codels: &Vec<Vec<Codel>>, point: (usize, usize)) -> HashSet<(usize, usize)> {
         let mut seen = HashSet::new();
         let mut members = HashSet::new();
         let mut neighbors = vec![point];
@@ -111,8 +93,7 @@ impl Program {
             }
         }
 
-        // TODO: there should be an easy way to ditch this clone
-        (Region::new(Vec::from_iter(members.clone())), members)
+        members
     }
 
     /// Get all the neighbors of a given point
